b88daffe41e92bc8eb4a1c8ec441ba04b6e61a24
andgreendigital <agreen@and.digital>
2022-12-04 08:30:32 +0000

    :sparkles: Day 3

--- src/day3.js
+++ src/day3.js
@@ -1,9 +1,57 @@
-function partOne(input) {
+const {
+  pipe, map, __, splitAt, innerJoin, equals, apply, head, ifElse, subtract, lt, sum, splitEvery,
+  reduce, converge, tail,
+} = require('ramda');
+const { splitInput } = require('./utils');
 
+function splitInHalf(string) {
+  return splitAt(string.length / 2, string);
+}
+
+const convertToPriority = pipe(
+  (character) => character.charCodeAt(),
+  ifElse(
+    lt(96),
+    subtract(__, 96),
+    subtract(__, 38),
+  ),
+);
+
+const innerJoinAll = converge(
+  reduce(
+    innerJoin(equals),
+  ),
+  [head, tail],
+);
+
+function partOne(input) {
+  return pipe(
+    splitInput,
+    map(
+      pipe(
+        splitInHalf,
+        apply(innerJoin(equals)),
+        head,
+        convertToPriority,
+      ),
+    ),
+    sum,
+  )(input);
 }
 
 function partTwo(input) {
-
+  return pipe(
+    splitInput,
+    splitEvery(3),
+    map(
+      pipe(
+        innerJoinAll,
+        head,
+        convertToPriority,
+      ),
+    ),
+    sum,
+  )(input);
 }
 
 module.exports = { partOne, partTwo };
--- src/day3.test.js
+++ src/day3.test.js
@@ -1,13 +1,17 @@
 const { partOne, partTwo } = require('./day3');
 
-const testData = `
-
+const testData = `vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw
 `;
 
 test('Part One', () => {
-  expect(partOne(testData)).toBe();
+  expect(partOne(testData)).toBe(157);
 });
 
 test('Part Two', () => {
-  expect(partTwo(testData)).toBe();
+  expect(partTwo(testData)).toBe(70);
 });
