454ff2de923b30ce4b2c8ffd0adbe70da9fa64fa
andgreendigital <agreen@and.digital>
2022-12-06 20:54:37 +0000

    :sparkles: Day 5

--- src/day5.js
+++ src/day5.js
@@ -1,9 +1,75 @@
-function partOne(input) {
+const {
+  split, pipe, head, init, map, splitEvery, transpose, nth, trim, match, zipObj, filter, isNil, not,
+  compose, useWith, apply, reduce, adjust, dec, evolve, repeat, join, slice, remove, concat,
+} = require('ramda');
 
+const notEmptySlot = compose(not, isNil);
+
+const parseCrates = pipe(
+  split('\n'),
+  init,
+  map(splitEvery(4)),
+  transpose,
+  map(pipe(
+    map(pipe(
+      match(/[A-Z]/),
+      head,
+    )),
+    filter(notEmptySlot),
+  )),
+);
+
+const parseInstructions = pipe(
+  trim,
+  split('\n'),
+  map(pipe(
+    match(/\d+/g),
+    zipObj(['numberOfCrates', 'fromStack', 'toStack']),
+    evolve({
+      fromStack: dec,
+      toStack: dec,
+    }),
+  )),
+);
+
+function moveCrates(numberOfCrates) {
+  return (stackState, { fromStack, toStack }) => pipe(
+    adjust(toStack, concat(slice(0, numberOfCrates, nth(fromStack, stackState)))),
+    adjust(fromStack, remove(0, numberOfCrates)),
+  )(stackState);
+}
+
+function crateMover9000(stackState, instruction) {
+  return reduce(moveCrates(1), stackState, repeat(instruction, instruction.numberOfCrates));
+}
+
+function crateMover9001(stackState, instruction) {
+  return moveCrates(instruction.numberOfCrates)(stackState, instruction);
+}
+
+function parseAndOperate(crateMover) {
+  return apply(useWith(reduce(crateMover), [parseCrates, parseInstructions]));
+}
+
+const topCrates = pipe(
+  map(head),
+  join(''),
+);
+
+function partOne(input) {
+  return pipe(
+    split('\n\n'),
+    parseAndOperate(crateMover9000),
+    topCrates,
+  )(input);
 }
 
 function partTwo(input) {
-
+  return pipe(
+    split('\n\n'),
+    parseAndOperate(crateMover9001),
+    topCrates,
+  )(input);
 }
 
 module.exports = { partOne, partTwo };
--- src/day5.test.js
+++ src/day5.test.js
@@ -1,13 +1,20 @@
 const { partOne, partTwo } = require('./day5');
 
-const testData = `
+const testData = `    [D]    
+[N] [C]    
+[Z] [M] [P]
+ 1   2   3 
 
+move 1 from 2 to 1
+move 3 from 1 to 3
+move 2 from 2 to 1
+move 1 from 1 to 2
 `;
 
 test('Part One', () => {
-  expect(partOne(testData)).toBe();
+  expect(partOne(testData)).toBe('CMZ');
 });
 
 test('Part Two', () => {
-  expect(partTwo(testData)).toBe();
+  expect(partTwo(testData)).toBe('MCD');
 });
