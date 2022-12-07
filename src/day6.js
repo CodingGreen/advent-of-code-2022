6c6d8a231c060aad0150033f60a8663ff1d21519
andgreendigital <agreen@and.digital>
2022-12-07 16:37:47 +0000

    :sparkles: Day 6

--- src/day6.js
+++ src/day6.js
@@ -1,9 +1,27 @@
-function partOne(input) {
+const {
+  pipe, aperture, findIndex, countBy, identity, values, add, all, equals,
+} = require('ramda');
 
+const hasNoDuplicates = pipe(
+  countBy(identity),
+  values,
+  all(equals(1)),
+);
+
+function detectMarker(numberOfDistinctCharacters, input) {
+  return pipe(
+    aperture(numberOfDistinctCharacters),
+    findIndex(hasNoDuplicates),
+    add(numberOfDistinctCharacters),
+  )(input);
+}
+
+function partOne(input) {
+  return detectMarker(4, input);
 }
 
 function partTwo(input) {
-
+  return detectMarker(14, input);
 }
 
 module.exports = { partOne, partTwo };
--- src/day6.test.js
+++ src/day6.test.js
@@ -1,13 +1,17 @@
 const { partOne, partTwo } = require('./day6');
 
-const testData = `
+describe.each([
+  ['mjqjpqmgbljsphdztnvjfqwrcgsmlb', 7, 19],
+  ['bvwbjplbgvbhsrlpgdmjqwftvncz', 5, 23],
+  ['nppdvjthqldpwncqszvftbrmjlhg', 6, 23],
+  ['nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg', 10, 29],
+  ['zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw', 11, 26],
+])('Test Case %#', (input, partOneExpectedOutput, partTwoExpectedOutput) => {
+  test('Part One', () => {
+    expect(partOne(input)).toBe(partOneExpectedOutput);
+  });
 
-`;
-
-test('Part One', () => {
-  expect(partOne(testData)).toBe();
-});
-
-test('Part Two', () => {
-  expect(partTwo(testData)).toBe();
+  test('Part Two', () => {
+    expect(partTwo(input)).toBe(partTwoExpectedOutput);
+  });
 });
