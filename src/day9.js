c05b6339b3183af4cf1c526132260a7a0cbcbb81
CodingGreen <17253156+CodingGreen@users.noreply.github.com>
2023-01-07 10:07:10 +0000

    Day 9 Part 1

--- src/day9.js
+++ src/day9.js
@@ -1,9 +1,87 @@
-function partOne(input) {
+const {
+  pipe, split, trim, map, lensProp, inc, over, dec, prop, zipWith, subtract, __, lensIndex, view,
+  all, clamp, add, set, range, zipObj, evolve, reduce, lensPath, append, uniq, length, ifElse,
+  always,
+} = require('ramda');
+const { between } = require('./utils');
 
+const xLens = lensIndex(0);
+const yLens = lensIndex(1);
+
+const movementActions = {
+  U: over(yLens, inc),
+  D: over(yLens, dec),
+  R: over(xLens, inc),
+  L: over(xLens, dec),
+};
+
+const getMovementAction = prop(__, movementActions);
+
+const coordinateDifference = zipWith(subtract);
+const coordinateAdd = zipWith(add);
+
+const parseInstructions = pipe(
+  trim,
+  split('\n'),
+  map(pipe(
+    split(' '),
+    zipObj(['direction', 'numberOfSteps']),
+    evolve({ numberOfSteps: Number }),
+  )),
+);
+
+const headPositionLens = lensPath(['currentPosition', 'headPosition']);
+const tailPositionLens = lensPath(['currentPosition', 'tailPosition']);
+const tailPositionHistoryLens = lensProp('tailPositionHistory');
+
+const withinTouchingDistance = between(-1, 1);
+
+const clampCoordinates = map(clamp(-1, 1));
+
+const moveTail = (state) => {
+  const tailPosition = view(tailPositionLens, state);
+  const diff = coordinateDifference(view(headPositionLens, state), tailPosition);
+  const newTailPosition = ifElse(
+    all(withinTouchingDistance),
+    always(tailPosition),
+    pipe(clampCoordinates, coordinateAdd(tailPosition)),
+  )(diff);
+  return pipe(
+    set(tailPositionLens, newTailPosition),
+    over(tailPositionHistoryLens, append(newTailPosition)),
+  )(state);
+};
+
+const moveInDirection = (direction) => {
+  const movementAction = getMovementAction(direction);
+  return pipe(
+    over(headPositionLens, movementAction),
+    moveTail,
+  );
+};
+
+const processAction = (state, action) => {
+  const move = moveInDirection(prop('direction', action));
+  return reduce(move, state, range(0, prop('numberOfSteps', action)));
+};
+
+function partOne(input) {
+  return pipe(
+    parseInstructions,
+    reduce(
+      processAction,
+      { currentPosition: { headPosition: [0, 0], tailPosition: [0, 0] }, tailPositionHistory: [] },
+    ),
+    view(tailPositionHistoryLens),
+    uniq,
+    length,
+  )(input);
 }
 
 function partTwo(input) {
 
 }
 
-module.exports = { partOne, partTwo };
+module.exports = {
+  partOne, partTwo, moveInDirection, moveTail,
+};
--- src/day9.test.js
+++ src/day9.test.js
@@ -1,11 +1,41 @@
-const { partOne, partTwo } = require('./day9');
+const {
+  partOne, partTwo, moveInDirection, moveTail,
+} = require('./day9');
 
-const testData = `
-
+const testData = `R 4
+U 4
+L 3
+D 1
+R 4
+D 1
+L 5
+R 2
 `;
 
+const createState = (headPos, tailPos) => ({
+  currentPosition: {
+    headPosition: [0, 0], tailPosition: [0, 0], ...headPos, ...tailPos,
+  },
+  tailPositionHistory: tailPos ? [tailPos.tailPosition] : [],
+});
+
+test('Move In Direction', () => {
+  expect(moveInDirection('U')(createState({ headPosition: [0, 1] })))
+    .toEqual(createState({ headPosition: [0, 2] }, { tailPosition: [0, 1] }));
+});
+
+test('Move Tail', () => {
+  expect(moveTail(createState({ headPosition: [2, 0] })))
+    .toEqual(createState({ headPosition: [2, 0] }, { tailPosition: [1, 0] }));
+});
+
+test('Move Tail 2', () => {
+  expect(moveTail(createState({ headPosition: [1, 2] })))
+    .toEqual(createState({ headPosition: [1, 2] }, { tailPosition: [1, 1] }));
+});
+
 test('Part One', () => {
-  expect(partOne(testData)).toBe();
+  expect(partOne(testData)).toBe(13);
 });
 
 test('Part Two', () => {
